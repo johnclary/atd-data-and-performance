08b91d44c5bd84eddbfb765ecdeffc45a5635463
John Clary <john.clary@austintexas.gov>
2021-09-19 10:40:36 -0500

    put cards in a single row group

--- pages/index.js
+++ pages/index.js
@@ -15,88 +15,84 @@ import {
   FaWrench,
 } from "react-icons/fa";
 
-const cards = {
-  signal_operations: [
-    {
-      title: "Signal Projects",
-      description:
-        "Get the latest info about active and upcoming traffic signal construction",
-      href: "/signal-projects",
-      img: { src: "/assets/data_and_performance.jpg", alt: "DTS logo" },
-      key: "signal_projects",
-      icon: <FaWrench />,
-    },
-    {
-      title: "Signal Monitor",
-      description: "Real-time monitoring of the City's traffic signals",
-      href: "/signal-monitor",
-      img: null,
-      key: "signal_monitor",
-      icon: <FaExclamationTriangle />,
-    },
-    {
-      title: "Traffic Cameras",
-      description: "Live images from the City's traffic cameras",
-      href: "/traffic-cameras",
-      img: null,
-      key: "traffic_cameras",
-      icon: <FaVideo />,
-    },
-    {
-      title: "Signal Timing",
-      description: "Track the progress of our annual signal re-timing work",
-      href: "/signal-timing",
-      img: null,
-      icon: <FaClock />,
-    },
-  ],
-  maps_resources: [
-    {
-      title: "Signal Assets",
-      description:
-        "Comprehensive map of traffig signal assets, including sensors, vehicle detectors, and school beacons.",
-      href: "/not-implemented",
-      img: null,
-    },
-    {
-      title: "Signs & Markings",
-      description:
-        "Info about signal requests where you can search and browse info.",
-      href: "/not-implemented-2",
-      img: null,
-    },
-    {
-      title: "Preventative Maintenance",
-      description:
-        "Info about signal requests where you can search and browse info.",
-      href: "/not-implemented-3",
-      img: null,
-    },
-  ],
-  open_data: [
-    {
-      title: "Shared Micromobility",
-      description:
-        "Info about signal requests where you can search and browse info.",
-      href: "/micromobility-data",
-      img: null,
-    },
-    {
-      title: "Data Catalog",
-      description:
-        "Info about signal requests where you can search and browse info.",
-      href: "/data-catalog",
-      img: null,
-    },
-    {
-      title: "City of Austin Github",
-      description:
-        "Info about signal requests where you can search and browse info.",
-      href: "/not-implemented-5",
-      img: null,
-    },
-  ],
-};
+const cards = [
+  {
+    title: "Signal Projects",
+    description:
+      "Get the latest info about active and upcoming traffic signal construction",
+    href: "/signal-projects",
+    img: { src: "/assets/data_and_performance.jpg", alt: "DTS logo" },
+    key: "signal_projects",
+    icon: <FaWrench />,
+  },
+  {
+    title: "Signal Monitor",
+    description: "Real-time monitoring of the City's traffic signals",
+    href: "/signal-monitor",
+    img: null,
+    key: "signal_monitor",
+    icon: <FaExclamationTriangle />,
+  },
+  {
+    title: "Traffic Cameras",
+    description: "Live images from the City's traffic cameras",
+    href: "/traffic-cameras",
+    img: null,
+    key: "traffic_cameras",
+    icon: <FaVideo />,
+  },
+  {
+    title: "Signal Timing",
+    description: "Track the progress of our annual signal re-timing work",
+    href: "/signal-timing",
+    img: null,
+    icon: <FaClock />,
+  },
+
+  {
+    title: "Signal Assets",
+    description:
+      "Comprehensive map of traffig signal assets, including sensors, vehicle detectors, and school beacons.",
+    href: "/not-implemented",
+    img: null,
+  },
+  {
+    title: "Signs & Markings",
+    description:
+      "Info about signal requests where you can search and browse info.",
+    href: "/not-implemented-2",
+    img: null,
+  },
+  {
+    title: "Preventative Maintenance",
+    description:
+      "Info about signal requests where you can search and browse info.",
+    href: "/not-implemented-3",
+    img: null,
+  },
+
+  {
+    title: "Shared Micromobility",
+    description:
+      "Info about signal requests where you can search and browse info.",
+    href: "/micromobility-data",
+    img: null,
+  },
+  {
+    title: "Data Catalog",
+    description:
+      "Info about signal requests where you can search and browse info.",
+    href: "/data-catalog",
+    img: null,
+  },
+  {
+    title: "City of Austin Github",
+    description:
+      "Info about signal requests where you can search and browse info.",
+    href: "/not-implemented-5",
+    img: null,
+  },
+];
 
 export function CardItem({ href, title, description, icon }) {
   return (
@@ -155,33 +151,8 @@ export default function Home() {
             </p>
           </Col>
         </Row>
-        <Row key="row-1" className="text-dts-4">
-          <Col>
-            <h3>Traffic Signal Operations</h3>
-          </Col>
-        </Row>
-        <Row key="row-2" className="text-dts-4 mb-4">
-          {cards.signal_operations.map((card) => {
-            return <CardItem key={card.href} {...card} />;
-          })}
-        </Row>
-        <Row key="row-3" className="text-dts-4">
-          <Col>
-            <h3>Maps & Resources</h3>
-          </Col>
-        </Row>
-        <Row key="row-4" className="text-dts-4 mb-4">
-          {cards.maps_resources.map((card) => {
-            return <CardItem key={card.href} {...card} />;
-          })}
-        </Row>
-        <Row key="row-5" className="text-dts-4">
-          <Col>
-            <h3>Open Data & Code</h3>
-          </Col>
-        </Row>
-        <Row key="row-6" className="text-dts-4 mb-4">
-          {cards.open_data.map((card) => {
+        <Row key="cards" className="text-dts-4 mb-4">
+          {cards.map((card) => {
             return <CardItem key={card.href} {...card} />;
           })}
         </Row>
